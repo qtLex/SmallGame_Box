018d272badb00ceb75c9150f34dc74da85b10d7e
Serj <mors@rarus.ru>
2015-02-01 18:47:00 +0300

    Изменена структура каталогов.

---
+++ Assets/Scripts/Animation/AlignToView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlignToView : MonoBehaviour {
+
+	private Camera _camera;
+
+	void Start () {
+		_camera = Camera.main;
+	}
+
+	void Update()
+	{
+		transform.LookAt(transform.position + _camera.transform.rotation * Vector3.back,
+		                 _camera.transform.rotation * Vector3.up);
+	}
+
+}
