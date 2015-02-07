c8cec28a88f273b5fe99110f6f08caf7dbb0b709
qtLex <qtlex@mail.ru>
2015-02-07 21:34:06 +0300

    новый кубик ловушка, предвтжение, блымалка

---
+++ Assets/Scripts/CubeClasses/TrapBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using BoxClasses;
+
+public class TrapBox : BaseBox {
+
+	private ParticleSystem _ps;
+	
+	public override void UserStay(){
+		if(!_ps){
+			_ps = GetComponentInChildren<ParticleSystem>();
+		}
+
+		if(!_ps.isPlaying)
+			_ps.Play(true);
+	}
+}
---
+++ Assets/Scripts/Player/psPlayerAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class psPlayerAction : MonoBehaviour {
+
+	private ParticleSystem psVer;
+	private ParticleSystem psHor;
+
+	public GameObject gpsver;
+	public GameObject gpshor;
+
+	// Use this for initialization
+	void Awake(){
+		Messenger.AddListener("UserAction", UserAction);
+
+		psVer = gpsver.GetComponent<ParticleSystem>();
+		psHor = gpshor.GetComponent<ParticleSystem>();
+	}
+	
+	public void UserAction(object sender, EventArgs evArgs){
+
+		Debug.Log("Проверка");
+
+		if(transform.up == Vector3.up || transform.up == -Vector3.up){
+			if(!psHor.isPlaying)
+				psHor.Play();
+		}
+		else{
+			if(!psVer.isPlaying)
+				psVer.Play();
+		}
+	}
+}
