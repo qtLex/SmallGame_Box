8c29a20f6cdd4eb118139d167b7c76815e415da0
qtLex <qtlex@mail.ru>
2015-03-09 01:25:16 +0300

    первый апгрейд для Unity3d_v5

--- Assets/Scripts/Camera/CameraHideObsticles.cs
+++ Assets/Scripts/Camera/CameraHideObsticles.cs
@@ -10,28 +10,34 @@ public class CameraHideObsticles : MonoBehaviour {
 		}
 	}
 
-	private float SourceAlpha = 0.0f;
-	private float DestAlpha = 0.9f;
+	//private float SourceAlpha = 0.0f;
+	//private float DestAlpha = 0.9f;
 
 	public Shader TransparencyShader;
 
 	void Update() {
 
-		if(!renderer){return;};
+		if(!GetComponent<Renderer>()){return;};
 
 		if (_isOnView){
 
-			renderer.material.shader = TransparencyShader;
+			//GetComponent<Renderer>().material.shader = TransparencyShader;
+			Color SourceAlpha = GetComponent<Renderer>().material.GetColor("_Color");
+			GetComponent<Renderer>().material.SetColor("_Color", new Color(SourceAlpha.r, SourceAlpha.g, SourceAlpha.b, Mathf.Lerp(SourceAlpha.a, 0f, 4 * Time.deltaTime)));
 
-			SourceAlpha = renderer.material.GetFloat("_AlphaTransparency");
-			DestAlpha = 0.9f;
-			renderer.material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
+			//SourceAlpha = GetComponent<Renderer>().material.GetFloat("_AlphaTransparency");
+			//DestAlpha = 0.9f;
+			//GetComponent<Renderer>().material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
 
-		}else{
+		}
+		else{
 
-			SourceAlpha = renderer.material.GetFloat("_AlphaTransparency");
-			DestAlpha = 0.0f;
-			renderer.material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
+			Color SourceAlpha = GetComponent<Renderer>().material.GetColor("_Color");
+			//Color DestAlpha = new Color(SourceAlpha.r, SourceAlpha.g, SourceAlpha.b, 255f);
+			GetComponent<Renderer>().material.SetColor("_Color", new Color(SourceAlpha.r, SourceAlpha.g, SourceAlpha.b, Mathf.Lerp(SourceAlpha.a, 255, 4 * Time.deltaTime)));
+			//float SourceAlpha = GetComponent<Renderer>().material.GetFloat("_AlphaTransparency");
+			//float DestAlpha = 0.0f;
+			//GetComponent<Renderer>().material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
 		}
 	
 	}
