bf874c386e5349dd6ab1d5385fb11721a2007f3e
qtLex <qtlex@mail.ru>
2015-03-22 02:24:45 +0300

    CameraHide воздействует и на дочерние объекты

--- Assets/Scripts/Camera/CameraHideObsticles.cs
+++ Assets/Scripts/Camera/CameraHideObsticles.cs
@@ -10,33 +10,61 @@ public class CameraHideObsticles : MonoBehaviour {
 		}
 	}
 
-	private Renderer _renderer;
+	private bool _initialized = false;
+	public bool Initialized{
+		set{
+			_initialized = value;
+		}
+	}
+
+	private Renderer   _renderer;
+	private Renderer[] _childrenRenderer;
+
 	private float SourceAlpha = 0.0f;
-	private float DestAlpha = 0.9f;
+	private float DestAlpha   = 0.9f;
 	
 	public Shader TransparencyShader;
-	
+
+	private bool Initialize(){
+		_renderer         = GetComponent<Renderer>();
+		_childrenRenderer = GetComponentsInChildren<Renderer>();
+
+		return true;
+	}
+
+	private void SetRendererAlpha(Renderer alphaItem){
+		if(!alphaItem || !alphaItem.enabled)
+			return;
+
+		if (_isOnView){
+			alphaItem.material.shader = TransparencyShader;
+			DestAlpha = 0.9f;
+		}
+		else{
+			DestAlpha = 0.0f;
+		}
+
+		Debug.Log(alphaItem);
+		
+		SourceAlpha = alphaItem.material.GetFloat("_AlphaTransparency");
+		float difference = Mathf.Abs(DestAlpha - SourceAlpha);
+
+		if(difference != 0){
+			float coef = (difference < 0.001f) ? 1f : 4 * Time.deltaTime;
+			alphaItem.material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, coef));
+		}
+	}
+
 	void Update() {
 
-		if(!_renderer)
-			_renderer = GetComponent<Renderer>();
+		if(!_initialized){
+			_initialized = Initialize();
+		}
 
-		if(!_renderer)
-			return;
-		
-		if (_isOnView){
-			
-			_renderer.material.shader = TransparencyShader;
-			
-			SourceAlpha = _renderer.material.GetFloat("_AlphaTransparency");
-			DestAlpha = 0.9f;
-			_renderer.material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
-			
-		}else{
-			
-			SourceAlpha = _renderer.material.GetFloat("_AlphaTransparency");
-			DestAlpha = 0.0f;
-			_renderer.material.SetFloat("_AlphaTransparency", Mathf.Lerp(SourceAlpha, DestAlpha, 4 * Time.deltaTime));
+		SetRendererAlpha(_renderer);
+
+		foreach(Renderer item in _childrenRenderer){
+			SetRendererAlpha(item);
 		}
 		
 	}
