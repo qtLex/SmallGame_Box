272fa8318a1afeb6f11117a38b9bffe4152dee97
qtLex <qtlex@mail.ru>
2015-08-24 23:20:37 +0300

    настройка нового куба

---
+++ Assets/Scripts/CubeClasses/WrapsBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using BoxClasses;
+using GameEnums;
+
+public class WrapsBox : BaseBox
+{
+    public override void UserAction(object sender, EventArgs evArgs)
+    {
+        
+    }
+}
