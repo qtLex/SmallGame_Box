67a67b2d665a7be818c500ff7d19d12d38af3ba4
qtLex <qtlex@mail.ru>
2015-09-05 16:52:26 +0300

    new files

---
+++ Assets/Scripts/ActionHistory/CancelActionWrapBox.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using BoxClasses;
+using GameEnums;
+using Motors.BoxMotors;
+
+public class CancelActionWrapBox : BaseCancelAction
+{
+    private BoxMotor motor;
+
+    private CubeGrid _grid;
+
+    void Awake()
+    {
+        _grid = GlobalOptions.Grid;
+        
+        motor = GetComponent<BoxMotor>();
+    }
+    
+    public override bool CancelAction(ActionHistoryType type, Vector3 lastDirection)
+    {
+        Debug.Log("Cancel: " + lastDirection);
+
+        // проверим возможность передвижения
+        if (lastDirection == Vector3.zero)
+            return false;
+
+        Vector3 NewPosition = new Vector3(lastDirection.x * _grid.gridSize + transform.position.x, lastDirection.y * _grid.gridSize + transform.position.y, lastDirection.z * _grid.gridSize + transform.position.z);
+        Vector3[] path = { NewPosition };
+        motor.path = path;
+
+        motor.StartMoving();
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Additionally/BoxMotor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Motors.BoxMotors
+{
+    public class BoxMotor : MonoBehaviour
+    {
+        public Vector3[] path;
+        public float movingTime;
+        private float currentTime;
+
+        private bool _isMoving = false;
+
+        private int currentPathIndex;
+        private Vector3 currentPosition;
+
+        private GameObject _player;
+        private Vector3 playerVector;
+        private PlayerController playerController;
+
+        public bool isMoving
+        {
+            get
+            {
+                return _isMoving;
+            }
+
+            set
+            {
+                _isMoving = value;
+                playerController.blockedByAnotherObject = value;
+            }
+        }
+
+        public bool StartMoving()
+        {
+            if (path.Length == 0)
+                return false;
+
+            currentPathIndex = 0;
+            currentPosition  = path[currentPathIndex];
+            currentTime      = movingTime;
+
+            isMoving = true;
+
+            playerVector = (_player.transform.position - transform.position);
+
+            return true;
+        }
+
+        void Start()
+        {
+            _player = GlobalOptions.Player;
+            playerController = _player.GetComponent<PlayerController>();
+        }
+
+        void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+            _player.transform.position = new Vector3(transform.position.x + playerVector.x, transform.position.y + playerVector.y, transform.position.z + playerVector.z);
+        }
+
+        void Update()
+        {
+            if (!isMoving)
+                return;
+
+            if (currentTime < 0)
+            {
+                SetPosition(currentPosition);
+
+                currentPathIndex++;
+
+                if (currentPathIndex >= path.Length)
+                {
+                    isMoving = false;
+                }
+                else
+                {
+                    currentPosition = path[currentPathIndex];
+                    currentTime = movingTime;
+                }
+
+                return;
+            }
+
+            SetPosition(Vector3.Lerp(transform.position, currentPosition, Time.deltaTime / currentTime));
+            currentTime -= Time.deltaTime;
+        }
+    }
+}
